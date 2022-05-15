6eb04a9e8f25cc13016a7d6fda443e451b090213
s.elmanov <s.elmanov@aventica.ru>
2022-05-15 11:16:50 +0300

    Add deploy

--- .github/workflows/scripts/deploy-ssh.sh
+++ .github/workflows/scripts/deploy-ssh.sh
@@ -8,7 +8,7 @@ echo -e [] ${GREEN}Docker pull new container
 sudo docker pull cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG
 
 echo -e [] Search running containers
-containerIds=$(sudo docker ps -a -q --filter "name=Frontend")
+containerIds=$(sudo docker ps -a -q --filter "name=Backend")
 
 echo -e [] Running containers: ${GREEN}$containerIds
 if [[ -n $containerIds ]] ; then
@@ -16,6 +16,6 @@ if [[ -n $containerIds ]] ; then
 fi
 
 echo -e [] Run ${GREEN}cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG ${NC}WITH NAME ${GREEN}Frontend.$GITHUB_RUN_NUMBER
-sudo docker run --name Frontend.$GITHUB_RUN_NUMBER -d -p $PORT:$PORT cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG
+sudo docker run --name Backend.$GITHUB_RUN_NUMBER -d -p $PORT:$PORT cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG
 
 sudo docker system prune -a -f
