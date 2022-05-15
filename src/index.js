aafa9d130374a1a5fe286b2f53ceda49d14c62ee
s.elmanov <s.elmanov@aventica.ru>
2022-05-15 11:54:23 +0300

    update connection mongo

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const { VM_HOST, BACKEND_PORT } = require('../config/config')
 async function start() {
   await connection()
 
-  app.listen(BACKEND_PORT, VM_HOST, () => {
-    console.log(`listen on ${VM_HOST}:${BACKEND_PORT}`)
+  app.listen(BACKEND_PORT, '0.0.0.0', () => {
+    console.log(`listen on 0.0.0.0:${BACKEND_PORT}`)
   })
 }
 
