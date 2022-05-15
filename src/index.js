493bb680847e706ce386794666f201ef4c63c1b7
s.elmanov <s.elmanov@aventica.ru>
2022-05-15 11:09:19 +0300

    Add deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const app = require('./app')
 const { connection } = require('./database/db')
 
+const { VM_HOST, BACKEND_PORT } = require('../config/config')
+
 async function start() {
   await connection()
 
-  app.listen(3001, () => {
-    console.log('listen on 3001')
+  app.listen(BACKEND_PORT, VM_HOST, () => {
+    console.log(`listen on ${VM_HOST}:${BACKEND_PORT}`)
   })
 }
 
